ddb4a694220a01a2734af7230ca426223429e0f0
Gaurang Jain <gjboxer19122001@gmail.com>
2024-01-16 11:52:48 +0530

    updated views.py for adding scientific operations

--- django_calculator/api/urls.py
+++ django_calculator/api/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("square_root/", views.square_root, name="square_root"),
+    path("power/", views.power, name="power"),
+    path("log/", views.log, name="log"),
+    path("log10/", views.log10, name="log10"),
+    path("sin/", views.sin, name="sin"),
+    path("cos/", views.cos, name="cos"),
+    path("tan/", views.tan, name="tan"),
+    path("factorial/", views.factorial, name="factorial"),
+    path("exponential/", views.exponential, name="exponential"),
 ]
